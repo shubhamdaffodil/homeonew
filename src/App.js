fbcf10cf21456bb90f85b45eabfb750745052ee6
shubham nautiyal <shubham.nautiyal@daffodilsw.com>
2022-10-19 18:16:28 +0530

    update removed

--- src/App.js
+++ src/App.js
@@ -6,21 +6,15 @@ import { useEffect } from 'react';
 
 
 function App() {
-  const UPDATE_CHECK_INTERVAL = 1 * 60 * 1000
-  useEffect(() => {
- 
-  }, [UPDATE_CHECK_INTERVAL])
 
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Chalega Gya Sala.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://synergy-homeopathic.com/"
           target="_blank"
           rel="noopener noreferrer"
         >
