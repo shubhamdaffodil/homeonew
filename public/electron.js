a45437d8529270ba462bf62bd3a76554f95d4906
shubham nautiyal <shubham.nautiyal@daffodilsw.com>
2023-01-12 15:05:09 +0530

    serial no added

--- public/electron.js
+++ public/electron.js
@@ -30,62 +30,53 @@ async function createWindow() {
       preload: path.join(__dirname, "preload.js"),
       devTools: false,
       nodeIntegration: true,
-      contextIsolation: true,
+      nodeIntegrationInWorker: true,
+      enableRemoteModule: true,
+      plugins: true,
+      contextIsolation: false,
+      webSecurity: false,
     },
   });
   if (process.platform !== "darwin") {
     win.setMenuBarVisibility(false);
   }
-  const { machineIdSync } = require("node-machine-id");
-  const machine_id = await machineIdSync({ original: true });
-  win.webContents.once("dom-ready", () => {
-    loadingwin.close();
-    win.maximize();
-    win.show();
+  var serialNumber = require('serial-number');
+  serialNumber(function (err, machine_id) {
+    win.webContents.once("dom-ready", () => {
+      loadingwin.close();
+      win.maximize();
+      win.show();
     win.webContents.executeJavaScript(
       `localStorage.setItem('token','${machine_id}')`
     );
   });
-  win.webContents.session.on("will-download", (event, item, webContents) => {
-    const fileName = item.getFilename();
-    if (fileName.includes("index-i")) {
-      let path;
-      if (fileName.includes("index-i0")) {
-        path = dialog.showOpenDialogSync({
-          message: "Select/create folder for backup location",
-          properties: ["openDirectory", "createDirectory", "promptToCreate"],
-          buttonLabel: "Backup",
-        })[0];
+});
 
-        downloadPath = path;
-      } else {
-        path = downloadPath;
-      }
-      path = `${path}\\${item.getFilename()}`;
-      path = path.slice(0, path.indexOf("index-i"));
-      item.setSavePath(path);
-    }
-  });
-
-  ipcMain.on("close-window", (event, data) => {
-    PreventClose = data;
-    win.close();
-  });
-  win.on("close", (event, data) => {
+ipcMain.on("close-window", (event, data) => {
+  PreventClose = data;
+  win.close();
+});
+ win.on("close", (event, data) => {
     if (PreventClose) {
       event.preventDefault();
       PreventClose = false;
       win && win.webContents.send("checkData");
     }
   });
-  startUrl = "https://web.synergy-homeopathic.com/";
+  // startUrl = "http://localhost:3000/";
+  // startUrl = "http://13.58.158.167:3006/";
+  // startUrl = "http://18.117.138.112:3000/";
+  // startUrl = "http://43.205.111.78:3000/";
+  // startUrl = "https://web.synergy-homeopathi.com/";
+  startUrl = "https://deweb.synergy-homeopathic.com/";
+  // startUrl = "https://en.synergy-homeopathic.com/";
   win.loadURL(startUrl);
 }
 
 app.whenReady().then(() => {
   createWindow();
 
-  app.on("activate", () => {
+  app.on("activate", () => {    
     if (BrowserWindow.getAllWindows().length === 0) {
       createWindow();
     }
